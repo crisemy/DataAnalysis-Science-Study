59f16da510231b91dc38d1030d932a769b48d30e
Cristian Nadj <cnadj@tdsi.com>
2026-01-31 09:33:10 -0300

    Adding a new Udemy Project

---
+++ Udemy/PythonAZForDataScienceWithRealExcercises/HomeworkPythonExcercise.py
@@ -0,0 +1,2 @@
+# Law of large numbers
+# LLN: Xn -->  E(X)  when n --> infinite and is the sample you are using for collecting data --> where X is average  --> where E = expected value
---
+++ Udemy/PythonAZForDataScienceWithRealExcercises/README.md
@@ -0,0 +1,5 @@
+# Course
+https://www.udemy.com/course/python-coding/learn/lecture/42632274?start=1#overview
+
+# Matplotlib page:
+https://matplotlib.org/
